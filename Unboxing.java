85622b2ec8f090acc456d5e71864dc4cbe5efd5a
Shweta chavan <146429359+ShwetaChavan595@users.noreply.github.com>
2025-03-05 09:17:34 +0530

    Add files via upload

---
+++ Unboxing.java
@@ -0,0 +1,25 @@
+// In the unboxing the wrapper class object is converted into the primitive data type
+class Unboxing 
+{
+public static void main(String args[])
+{
+Integer a=50;
+int num=a;
+System.out.println("The value of number is "+num);
+
+Byte b1=23;
+byte b2=b1;
+System.out.println("The value of b2  is "+b2);
+
+
+
+
+
+
+
+
+
+
+}
+}
+  
