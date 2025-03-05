f681d223301609429a5f8c89b501a67e96e1777d
Shweta chavan <146429359+ShwetaChavan595@users.noreply.github.com>
2025-03-05 09:17:04 +0530

    Add files via upload

---
+++ Autoboxing.java
@@ -0,0 +1,24 @@
+//In this autoboxing the primitive datatype are converted into its corrsponding wrapper class 
+
+class Autoboxing{
+public static void main(String args[])
+{
+int num=10;
+Integer obj=num;
+System.out.println("value of obj is "+obj);
+
+double d1=29.78;
+Double d2=d1;
+System.out.println("value of d2 is "+d2);
+
+char c1='e';
+Character c2=c1;
+System.out.println("value of c2  is "+c2);
+byte b1=127;
+Byte b2=b1;
+System.out.println("value of b2  is "+b2);
+
+
+
+}
+}
